c154f3b9816bedd7544df41bb59a2382a6303381
Matthew H. Sears <mattsears18@gmail.com>
2018-03-24 12:37:54 -0600

    Datafiles.process Method initial tests

--- collections/Datafiles/Hooks.js
+++ collections/Datafiles/Hooks.js
@@ -1,7 +1,4 @@
 Datafiles.collection.after.remove(function (userId, datafile) {
-  console.log('after update:');
-  console.log(datafile);
-
   Recordings.remove({datafileId: datafile._id});
   //TODO remove datafileId from all AOIs
   Aois.update({studyId: datafile.studyId}, {$pull: {datafileIds: datafile._id}});
---
+++ collections/Studies/Studies.test.js
@@ -0,0 +1,40 @@
+import Schemas from '../../lib/schemas';
+import Studies from './Studies';
+
+// describe('foo is 5', function () {
+//   foo = 5;
+//
+//   studies = Studies.find({});
+//
+//   it('foo equals 5', function () {
+//     chai.expect(foo).to.equal(5);
+//   });
+//
+//   it('foo does not equal 17', function () {
+//     chai.expect(foo).to.not.equal(17);
+//   });
+// });
+
+
+// describe('Studies', function() {
+//   describe('Permissions', function() {
+//     var randomEmail = faker.internet.email();
+//     var randomName = faker.name.findName();
+//     var userName = faker.internet.userName();
+//     Accounts.createUser({
+//       username: userName,
+//       profile: {
+//         name: randomName,
+//       },
+//       email: randomEmail,
+//       password: 'password'
+//     });
+//
+//     user = Meteor.users.findOne();
+//     console.log(user);
+//
+//     it('gives the current user permission to update', function(){
+//
+//     });
+//   });
+// });

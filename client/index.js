d12f22a23f8e7850b3cf7d229764b45d8adeb190
mattsears18 <mattsears18@gmail.com>
2018-08-21 13:26:25 -0400

    fix datatables style

--- client/index.js
+++ client/index.js
@@ -1,6 +1,10 @@
 import 'select2';
 import 'select2/dist/css/select2.css';
 import 'select2-bootstrap-theme/dist/select2-bootstrap.css';
+import { $ } from 'meteor/jquery';
+import dataTablesBootstrap from 'datatables.net-bs';
+import 'datatables.net-bs/css/dataTables.bootstrap.css';
+dataTablesBootstrap(window, $);
 
 // Make helpers available on Templates
 for (helper in helpers) {

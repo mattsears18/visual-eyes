84a2dae63c27c4ae5e323803e8c65ac6b05a422b
mattsears18 <mattsears18@gmail.com>
2018-10-11 18:37:25 -0600

    working animations, still need centroid trace

--- client/partials/animations/convexHulls/instantaneous.js
+++ client/partials/animations/convexHulls/instantaneous.js
@@ -13,11 +13,7 @@ Template.ConvexHullInstantaneous.onCreated(function() {
         viewing = Viewings.findOne(viewingId);
 
         if(viewing) {
-          hulls = viewing.getHulls();
-
-          hulls.forEach(function(hull) {
-            plotHull(hull);
-          });
+          plotInit(viewing);
         }
       }
     }
@@ -71,53 +67,29 @@ Template.ConvexHullInstantaneous.helpers({
 //   },
 // });
 
+var pointsTrace = {};
+var polygonTrace = {};
+var centroidTrace = {};
+var centroidHistoryTrace = {};
+var centroids = [];
+var lastFixationTrace = {};
+var lastFixationPastTrace = {};
+var lastFixationLength;
+var data;
+var layout;
+
 function plotInit(viewing) {
-  console.log('plot init');
-}
+  lastFixationLength = 5;
 
-function plotAnimate() {
-  console.log('plot animate');
-}
+  data = [
+    pointsTrace,
+    polygonTrace,
+    centroidTrace,
+    centroidHistoryTrace,
+    lastFixationTrace,
+    lastFixationPastTrace,
+  ];
 
-
-function plotHull(hull) {
-  var pointsTrace = {
-    name: 'Fixation',
-    x: hull.pointsXY.map(function(point) { return point.x; }),
-    y: hull.pointsXY.map(function(point) { return point.y; }),
-    mode: 'markers',
-    type: 'scatter',
-    marker: {
-      color: '#337ab7',
-      size: 8,
-    },
-  };
-
-  var polygonTrace = {
-    name: 'Convex Hull',
-    x: hull.polygonXY.map(function(point) { return point.x; }),
-    y: hull.polygonXY.map(function(point) { return point.y; }),
-    mode: 'lines',
-    type: 'scatter',
-    line: {
-      color: '#000000',
-      width: 2.5,
-    },
-  };
-
-  var centroidTrace = {
-    name: 'Centroid',
-    x: [hull.centroid.x, hull.centroid.x], //repeat twice or it won't plot
-    y: [hull.centroid.y, hull.centroid.y],
-    mode: 'markers',
-    type: 'scatter',
-    marker: {
-      color: '#dc3545',
-      size: 8,
-    },
-  };
-
-  data = [pointsTrace, polygonTrace, centroidTrace];
   layout = {};
 
   if(viewing.aoi() && viewing.aoi().image()) {
@@ -157,37 +129,114 @@ function plotHull(hull) {
     data,
     layout,
   );
+
+  hulls = viewing.getHulls();
+
+  hulls.forEach(function(hull) {
+    plotAnimate(hull);
+  });
 }
 
-//
-// Plotly.plot('graph', [{
-//   x: [0, 0],
-//   y: [-1, 1],
-// }], {
-//   xaxis: {range: [-Math.PI, Math.PI]},
-//   yaxis: {range: [-1.3, 1.3]}
-// }).then(function () {
-//   Plotly.addFrames('graph', [
-//     {
-//       data: [{x: [1, -1], y: [0, 0]}],
-//       name: 'frame1'
-//     }, {
-//       data: [{x: [0, 0], y: [-1, 1]}],
-//       name: 'frame2'
-//     }
-//   ]);
-// })
-//
-// function startAnimation() {
-//   Plotly.animate('graph', ['frame1', 'frame2'], {
-//     frame: [
-//       {duration: 1500},
-//       {duration: 500},
-//     ],
-//     transition: [
-//       {duration: 800, easing: 'elastic-in'},
-//       {duration: 100, easing: 'cubic-in'},
-//     ],
-//     mode: 'afterall'
-//   })
-// }
+function getLastFixationHistory(hull) {
+  points = hull.pointsXY.slice(Math.max(0, hull.pointsXY.length - lastFixationLength));
+
+  return {
+    x: points.map(function(point) { return point.x; }),
+    y: points.map(function(point) { return point.y; }),
+  }
+}
+
+function plotAnimate(hull) {
+  pointsTrace = {
+    name: 'Fixation',
+    x: hull.pointsXY.map(function(point) { return point.x; }),
+    y: hull.pointsXY.map(function(point) { return point.y; }),
+    mode: 'markers',
+    type: 'scatter',
+    marker: {
+      color: '#337ab7',
+      size: 8,
+    },
+  };
+
+  polygonTrace = {
+    name: 'Convex Hull',
+    x: hull.polygonXY.map(function(point) { return point.x; }),
+    y: hull.polygonXY.map(function(point) { return point.y; }),
+    mode: 'lines',
+    type: 'scatter',
+    line: {
+      color: '#000000',
+      width: 2.5,
+    },
+  };
+
+  centroidTrace = {
+    name: 'Centroid',
+    x: [hull.centroid.x, hull.centroid.x], //repeat twice or it won't plot
+    y: [hull.centroid.y, hull.centroid.y],
+    mode: 'markers',
+    type: 'scatter',
+    marker: {
+      color: '#dc3545',
+      size: 8,
+    },
+  };
+
+  // centroidHistoryTrace = {
+  //   name: 'Centroid History',
+  //   x: getCentroidHistory(hull).x,
+  //   y: getCentroidHistory(hull).y,
+  //   mode: 'lines',
+  //   type: 'scatter',
+  //   line: {
+  //     color: '#dc3545',
+  //     width: 2.5,
+  //   },
+  // };
+
+  lastFixationTrace = {
+    name: 'Last Fixation',
+    x: [hull.pointsXY[(hull.pointsXY.length - 1)].x],
+    y: [hull.pointsXY[(hull.pointsXY.length - 1)].y],
+    mode: 'markers',
+    type: 'scatter',
+    marker: {
+      color: '#63a70a',
+      size: 15,
+    },
+  };
+
+  lastFixationPastTrace = {
+    name: 'Last ' + lastFixationLength + ' Fixations',
+    x: getLastFixationHistory(hull).x,
+    y: getLastFixationHistory(hull).y,
+    mode: 'lines',
+    type: 'scatter',
+    line: {
+      // color: '#dc3545',
+      color: '#63a70a',
+      width: 2.5,
+    },
+  };
+
+  data = [
+    pointsTrace,
+    polygonTrace,
+    centroidTrace,
+    centroidHistoryTrace,
+    lastFixationTrace,
+    lastFixationPastTrace,
+  ];
+
+  Plotly.animate('ConvexHullInstantaneous-Viewing', {
+    data: data,
+  }, {
+    transition: {
+      duration: 0,
+    },
+    frame: {
+      duration: hull.timeStep,
+    }
+  })
+}

457dd241b4c05af6377e1e4549c196e752051843
mattsears18 <mattsears18@gmail.com>
2018-12-26 14:00:57 -0500

    before refactoring getHulls()

--- collections/Viewings/Hooks.js
+++ collections/Viewings/Hooks.js
@@ -5,5 +5,9 @@ Viewings.before.insert(function (userId, doc) {
 
   doc.minRecordingTime = Meteor.call('viewings.getMinTime', { viewing: doc });
   doc.maxRecordingTime = Meteor.call('viewings.getMaxTime', { viewing: doc });
+  doc.averageSlideHullSize = Meteor.call('viewings.getAverageSlideHullSize', { viewing: doc });
+  doc.averageStepHullSize = Meteor.call('viewings.getAverageStepHullSize', { viewing: doc });
+  doc.slideHulls =  Meteor.call('viewings.getSlideHulls', { viewing: doc });
+  doc.stepHulls =  Meteor.call('viewings.getStepHulls', { viewing: doc });
   doc.duration = doc.maxRecordingTime - doc.minRecordingTime;
 });

0d76c33e875ff01c574f0f5789b6a10e4c5f743c
mattsears18 <mattsears18@gmail.com>
2018-12-31 11:28:04 -0500

    before fixing timeStep calculation

--- collections/Viewings/Hooks.js
+++ collections/Viewings/Hooks.js
@@ -5,8 +5,8 @@ Viewings.before.insert(function (userId, doc) {
 
   doc.minRecordingTime = Meteor.call('viewings.getMinTime', { viewing: doc });
   doc.maxRecordingTime = Meteor.call('viewings.getMaxTime', { viewing: doc });
-  doc.averageSlideHullSize = Meteor.call('viewings.getAverageSlideHullSize', { viewing: doc });
-  doc.averageStepHullSize = Meteor.call('viewings.getAverageStepHullSize', { viewing: doc });
+  // doc.averageSlideHullSize = Meteor.call('viewings.saveAverageSlideHullSize', { viewing: doc });
+  // doc.averageStepHullSize = Meteor.call('viewings.saveAverageStepHullSize', { viewing: doc });
   doc.slideHulls =  Meteor.call('viewings.getSlideHulls', { viewing: doc });
   doc.stepHulls =  Meteor.call('viewings.getStepHulls', { viewing: doc });
   doc.duration = doc.maxRecordingTime - doc.minRecordingTime;
---
+++ server/workers/viewings/saveAverageSlideHullSize.js
@@ -0,0 +1,10 @@
+import Jobs from '../../../collections/Jobs/Jobs';
+
+export default queueViewingsSaveAverageSlideHullSize = Jobs.processJobs('viewings.saveAverageSlideHullSize',
+  function (jobDoc, callback) {
+    Meteor.call('viewings.saveAverageSlideHullSize', {
+      jobId: jobDoc._doc._id,
+      callback: callback,
+    });
+  }
+);

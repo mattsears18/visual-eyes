73ee506b567e9e75b50e993df3200604991f7489
mattsears18 <mattsears18@gmail.com>
2019-03-21 13:28:56 -0400

    viewings.saveCSV method

--- client/partials/animations/animation.js
+++ client/partials/animations/animation.js
@@ -13,3 +13,15 @@ Template.Animation.onCreated(function() {
 Template.Animation.helpers({
   stimulus: () => { return Stimuli.findOne(); },
 });
+
+Template.Animation.events({
+  'click .download-hulls-as-csv': (e, template) => {
+    Meteor.call('viewings.saveCSV', {
+      viewingId: FlowRouter.getParam('viewingId'),
+      analysisType: Session.get('analysisType'),
+      instantContinuous: Session.get('instantContinuous'),
+      slideStep: Session.get('slideStep'),
+      centroidPeriod: Session.get('centroidPeriod'),
+    });
+  },
+});

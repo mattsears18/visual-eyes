ad4bd7647515919381f67934fa7b80f545c7f170
mattsears18 <mattsears18@gmail.com>
2019-04-03 22:42:42 -0600

    more tests

--- collections/Datafiles/helpers/getNumericPositiveCoordinatesOnly.test.js
+++ collections/Datafiles/helpers/getNumericPositiveCoordinatesOnly.test.js
@@ -4,5 +4,42 @@ import { Factory } from 'meteor/dburles:factory';
 require('./../../factories');
 
 describe('Datafiles.getNumericPositiveCoordinatesOnly()', () => {
-  
+  it('removes negative coordinate values', () => {
+    let datafile = Factory.create('imotionsDatafile');
+    let rows = [
+      { x: '100', y: '500'},
+      { x: '-1', y: '500'},
+      { x: '100', y: '-1'},
+    ];
+    let expectedRows = [
+      { x: '100', y: '500'},
+    ];
+    chai.expect(datafile.getNumericPositiveCoordinatesOnly(rows)).to.eql(expectedRows);
+  });
+
+  it('removes blanks', () => {
+    let datafile = Factory.create('imotionsDatafile');
+    let rows = [
+      { x: '100', y: '500'},
+      { x: '', y: '500'},
+      { x: '100', y: ''},
+    ];
+    let expectedRows = [
+      { x: '100', y: '500'},
+    ];
+    chai.expect(datafile.getNumericPositiveCoordinatesOnly(rows)).to.eql(expectedRows);
+  });
+
+  it('removes dashes', () => {
+    let datafile = Factory.create('imotionsDatafile');
+    let rows = [
+      { x: '100', y: '500'},
+      { x: '-', y: '500'},
+      { x: '100', y: '-'},
+    ];
+    let expectedRows = [
+      { x: '100', y: '500'},
+    ];
+    chai.expect(datafile.getNumericPositiveCoordinatesOnly(rows)).to.eql(expectedRows);
+  });
 });
--- collections/Datafiles/helpers/getRenamedRows.test.js
+++ collections/Datafiles/helpers/getRenamedRows.test.js
@@ -1,8 +1,8 @@
-import Datafiles from './../Datafiles';
-import { resetDatabase } from 'meteor/xolvio:cleaner';
-import { Factory } from 'meteor/dburles:factory';
-require('./../../factories');
-
-describe('Datafiles.getRenamedRows()', () => {
-  
-});
+// import Datafiles from './../Datafiles';
+// import { resetDatabase } from 'meteor/xolvio:cleaner';
+// import { Factory } from 'meteor/dburles:factory';
+// require('./../../factories');
+//
+// describe('Datafiles.getRenamedRows()', () => {
+//
+// });

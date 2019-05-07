36ffc6c274f48a5576a8e9e36c6a74f1420f2151
mattsears18 <mattsears18@gmail.com>
2019-05-07 18:30:22 -0400

    dev settings

--- server/init.js
+++ server/init.js
@@ -1,6 +1,8 @@
-import Jobs from '../collections/Jobs/Jobs';
 import jst from 'jStat'; // can't write import jStat from 'jStat' as it would override the jStat variable on the client and set it to undefined
+import Jobs from '../collections/Jobs/Jobs';
 const os = require('os');
+
+require('../devSettings');
 
 Meteor.startup(() => {
   Jobs.startJobServer();
@@ -14,57 +16,58 @@ Meteor.startup(() => {
   });
 });
 
-if (Meteor.isServer){
+if (Meteor.isServer) {
   jStat = jst.jStat;
   fs = Npm.require('fs');
 
   // Make '/data/meteor/uploads' writable by user
   // fs.chmodSync('/data/meteor/uploads', 0o755);
 
-  if(!Meteor.settings.public.uploads) {
+  if (!Meteor.settings.public.uploads) {
     console.log('no uploads directory set in Meteor.settings');
 
-    defaultUploadsDir = os.homedir() + '/data/meteor/uploads';
-    console.log('set uploads directory to: ' + defaultUploadsDir);
+    defaultUploadsDir = `${os.homedir()}/data/meteor/uploads`;
+    console.log(`set uploads directory to: ${defaultUploadsDir}`);
     Meteor.settings.public.uploads = defaultUploadsDir;
 
-    settingsFilepath = process.env.PWD + '/settings.json';
+    settingsFilepath = `${process.env.PWD}/settings.json`;
 
     if (fs.existsSync(settingsFilepath)) {
       console.log('settings file exists, check for uploads directory');
       fs.readFile(settingsFilepath, 'utf-8', (err, data) => {
-        if(err) {
+        if (err) {
           console.log(err);
         } else {
           try {
             settings = JSON.parse(data);
-            if(!settings.public.uploads) {
+            if (!settings.public.uploads) {
               // No uploads directory set, set to default
               console.log('no uploads directory set');
-              console.log('set uploads directory to: ' + defaultUploadsDir);
+              console.log(`set uploads directory to: ${defaultUploadsDir}`);
               settings.public.uploads = defaultUploadsDir;
               saveSettings(settingsFilepath, settings);
             } else {
               console.log('uploads directory already set in ./settings.json');
             }
-          }
-          catch(err) {
-            console.log('error reading settings.json, save new file with just the uploads directory setting');
-            settings = {public: {uploads: defaultUploadsDir}};
+          } catch (err) {
+            console.log(
+              'error reading settings.json, save new file with just the uploads directory setting',
+            );
+            settings = { public: { uploads: defaultUploadsDir } };
             saveSettings(settingsFilepath, settings);
           }
         }
       });
     } else {
-      settings = {public: {uploads: defaultUploadsDir}};
+      settings = { public: { uploads: defaultUploadsDir } };
       saveSettings(settingsFilepath, settings);
     }
   }
 }
 
 function saveSettings(settingsFilepath, settings) {
-  fs.writeFile(settingsFilepath, JSON.stringify(settings, null, 2), (err) => {
-    if(err) {
+  fs.writeFile(settingsFilepath, JSON.stringify(settings, null, 2), err => {
+    if (err) {
       console.log(err);
     } else {
       console.log('update settings.json');

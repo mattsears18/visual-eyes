93a4bc6a8d4d017e7da003068cf7d6b20e8997ac
mattsears18 <mattsears18@gmail.com>
2019-07-25 11:46:46 -0400

    change default analyses and only create them if they don't already exist

--- collections/Studies/imports/makeDefaultAnalyses.js
+++ collections/Studies/imports/makeDefaultAnalyses.js
@@ -1,30 +1,56 @@
 export default function makeDefaultAnalyses() {
-  const maxGlanceGapDurationDurations = [120, 250, 500, 1000, 2000, 3000, 4000, 5000];
-  const minGlanceDurations = [
+  const maxGlanceGapDurationDurations = [
+    60,
+    80,
+    100,
     120,
     250,
-    500,
-    1000,
-    2000,
-    3000,
-    4000,
-    5000,
-    10000,
-    20000,
-    30000,
+    // 500,
+    // 1000,
+    // 2000,
+    // 3000,
+    // 4000,
+    // 5000,
+  ];
+  const minGlanceDurations = [
+    60,
+    80,
+    100,
+    120,
+    250,
+    // 500,
+    // 1000,
+    // 2000,
+    // 3000,
+    // 4000,
+    // 5000,
+    // 10000,
+    // 20000,
+    // 30000,
   ];
 
+  const existingAnalyses = Analyses.find({ studyId: this._id }).fetch();
+
   minGlanceDurations.forEach((minGlanceDuration) => {
-    maxGlanceGapDurationDurations.forEach((maxGlanceGapDurationDuration) => {
-      Analyses.insert({
-        studyId: this._id,
-        name: 'Analysis',
-        maxGlanceGapDuration: maxGlanceGapDurationDuration,
-        minGlanceDuration: minGlanceDuration,
-        ignoreOutsideImage: true,
-        participantIds: this.participants().map(participant => participant._id),
-        stimulusIds: this.stimuli().map(stimulus => stimulus._id),
-      });
+    maxGlanceGapDurationDurations.forEach((maxGlanceGapDuration) => {
+      const matches = existingAnalyses.find(
+        analysis => analysis.minGlanceDuration === minGlanceDuration
+          && analysis.maxGlanceGapDuration === maxGlanceGapDuration,
+      );
+
+      if (typeof matches === 'undefined') {
+        Analyses.insert({
+          studyId: this._id,
+          name: 'Analysis',
+          maxGlanceGapDuration,
+          minGlanceDuration,
+          ignoreOutsideImage: true,
+          participantIds: this.participants().map(
+            participant => participant._id,
+          ),
+          stimulusIds: this.stimuli().map(stimulus => stimulus._id),
+        });
+      }
     });
   });
 }

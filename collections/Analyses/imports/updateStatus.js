a33de9e2b0a52612dda0896b4b94c211b66989c1
mattsears18 <mattsears18@gmail.com>
2019-08-02 08:16:50 -0400

    bug fix

--- collections/Analyses/imports/updateStatus.js
+++ collections/Analyses/imports/updateStatus.js
@@ -3,19 +3,23 @@ import { jStat } from 'jStat';
 export default function updateStatus() {
   if (this.allGlancesProcessed() && this.status != 'processed') {
     const glances = this.glances().fetch();
-
     const glanceCount = glances.length;
-    const glanceDurationMean = jStat.mean(
-      glances.map(function(glance) {
-        return glance.duration;
-      }),
-    );
+    let glanceDurationMean = 0;
+    let glanceDurationMedian = 0;
 
-    const glanceDurationMedian = jStat.median(
-      glances.map(function(glance) {
-        return glance.duration;
-      }),
-    );
+    if (glanceCount > 0) {
+      glanceDurationMean = jStat.mean(
+        glances.map(function(glance) {
+          return glance.duration;
+        }),
+      );
+
+      glanceDurationMedian = jStat.median(
+        glances.map(function(glance) {
+          return glance.duration;
+        }),
+      );
+    }
 
     Analyses.update(
       { _id: this._id },

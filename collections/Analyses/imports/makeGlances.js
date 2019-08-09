7c3a2bade7feca7b27acd7d65a4c31d0a22175a7
mattsears18 <mattsears18@gmail.com>
2019-08-09 09:15:39 -0400

    fix imports

--- collections/Analyses/imports/makeGlances.js
+++ collections/Analyses/imports/makeGlances.js
@@ -1,4 +1,6 @@
-import Analyses from '../Analyses';
+import Participants from '../../Participants/Participants';
+import Stimuli from '../../Stimuli/Stimuli';
+import Gazepoints from '../../Gazepoints/Gazepoints';
 
 export default function makeGlances({ participantId, stimulusId }) {
   if (!participantId) {
@@ -63,51 +65,52 @@ export default function makeGlances({ participantId, stimulusId }) {
   console.log('pick back up here!');
   // TODO pick back up here
 
-  // if (allGazepointsArr.length) {
-  //   const fileFormatGroups = _.groupBy(allGazepointsArr, 'fileFormat');
-  //   Object.keys(fileFormatGroups).forEach((fileFormat) => {
-  //     const gazepointsArr = fileFormatGroups[fileFormat];
-  //     const startIndex = 0;
-  //     const number = 1;
-  //     do {
-  //       let endIndex;
-  //       try {
-  //         endIndex = this.getGlanceEndIndex({
-  //           gazepoints: gazepointsArr,
-  //           startIndex,
-  //         });
-  //         // console.log('start: ' + startIndex + ' end: ' + endIndex);
-  //       } catch (err) {
-  //         // console.log('start: ' + startIndex + ' end: ' + endIndex);
-  //         if (err.error === 'minGlanceDurationNotMet') {
-  //           // console.log(err.details);
-  //           startIndex = err.details.nextIndex;
-  //         } else {
-  //           console.log(err);
-  //         }
-  //       }
-  //       if (!endIndex) {
-  //         continue;
-  //       }
-  //       try {
-  //         // console.log('make the glance!');
-  //         const glanceId = this.makeGlanceFromGazepoints({
-  //           gazepoints: gazepointsArr,
-  //           startIndex,
-  //           endIndex,
-  //           number: number++,
-  //           participantId,
-  //           stimulusId,
-  //           fileFormat,
-  //         });
-  //         glanceIds.push(glanceId);
-  //       } catch (err) {
-  //         console.log(err);
-  //       }
-  //       startIndex = endIndex + 1;
-  //     } while (startIndex < gazepointsArr.length - 1);
-  //   });
-  // }
+  if (allGazepointsArr.length) {
+    const fileFormatGroups = _.groupBy(allGazepointsArr, 'fileFormat');
+    Object.keys(fileFormatGroups).forEach((fileFormat) => {
+      const gazepointsArr = fileFormatGroups[fileFormat];
+      let startIndex = 0;
+      let number = 1;
+      do {
+        let endIndex;
+        try {
+          endIndex = this.getGlanceEndIndex({
+            gazepoints: gazepointsArr,
+            startIndex,
+          });
+        } catch (err) {
+          if (err.error === 'minGlanceDurationNotMet') {
+            console.log(err.details);
+            startIndex = err.details.nextIndex;
+          } else {
+            console.log(err);
+          }
+        } finally {
+          console.log(`start: ${startIndex} end: ${endIndex}`);
+        }
+
+        if (endIndex) {
+          try {
+            console.log('make the glance!');
+            const glanceId = this.makeGlanceFromGazepoints({
+              gazepoints: gazepointsArr,
+              startIndex,
+              endIndex,
+              number: (number += 1),
+              participantId,
+              stimulusId,
+              fileFormat,
+            });
+            glanceIds.push(glanceId);
+          } catch (err) {
+            console.log(err);
+          }
+        }
+
+        startIndex = endIndex + 1;
+      } while (startIndex < gazepointsArr.length - 1);
+    });
+  }
 
   return glanceIds;
 }

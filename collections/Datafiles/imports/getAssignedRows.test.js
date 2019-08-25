548dea3a28d0594022c5026437bf03c943ac8022
mattsears18 <mattsears18@gmail.com>
2019-08-24 18:30:32 -0600

    Datafiles.getAssignedRows() working

--- collections/Datafiles/imports/getAssignedRows.test.js
+++ collections/Datafiles/imports/getAssignedRows.test.js
@@ -3,7 +3,7 @@ import { Factory } from 'meteor/dburles:factory';
 require('../../factories.test');
 const { expect } = require('chai');
 
-describe.only('Datafiles.getAssignedRows()', () => {
+describe('Datafiles.getAssignedRows()', () => {
   it("doesn't pass any rawCSVData", () => {
     const datafile = Factory.create('imotionsDatafile');
     expect(() => {
@@ -15,6 +15,12 @@ describe.only('Datafiles.getAssignedRows()', () => {
     const datafile = Factory.create('imotionsDatafile');
     datafile.fileFormat = 'imotions';
     const rawCSVData = await datafile.getRawCSV();
+
+    expect(rawCSVData[0].stimulusId).to.be.undefined;
+    expect(rawCSVData[0].aoiId).to.be.undefined;
+    expect(rawCSVData[rawCSVData.length - 1].stimulusId).to.be.undefined;
+    expect(rawCSVData[rawCSVData.length - 1].aoiId).to.be.undefined;
+
     const assignedRows = datafile.getAssignedRows(rawCSVData);
 
     expect(assignedRows.length).to.equal(5290); // verified in excel
@@ -24,12 +30,23 @@ describe.only('Datafiles.getAssignedRows()', () => {
 
     const aois = Aois.find({ studyId: datafile.studyId });
     expect(aois.count()).to.equal(1);
+
+    expect(assignedRows[0].stimulusId).to.exist;
+    expect(assignedRows[0].aoiId).to.exist;
+    expect(assignedRows[assignedRows.length - 1].stimulusId).to.exist;
+    expect(assignedRows[assignedRows.length - 1].aoiId).to.exist;
   });
 
   it('assigns stimuli and aois to each row in a real smi file', async () => {
     const datafile = Factory.create('smiDatafile');
     datafile.fileFormat = 'smi';
     const rawCSVData = await datafile.getRawCSV();
+
+    expect(rawCSVData[0].stimulusId).to.be.undefined;
+    expect(rawCSVData[0].aoiId).to.be.undefined;
+    expect(rawCSVData[rawCSVData.length - 1].stimulusId).to.be.undefined;
+    expect(rawCSVData[rawCSVData.length - 1].aoiId).to.be.undefined;
+
     const assignedRows = datafile.getAssignedRows(rawCSVData);
 
     expect(assignedRows.length).to.equal(5761); // verified in excel
@@ -40,16 +57,37 @@ describe.only('Datafiles.getAssignedRows()', () => {
     const aois = Aois.find({ studyId: datafile.studyId });
 
     expect(aois.count()).to.equal(2); // '-' and 'ImageA' verified in excel
+
+    expect(assignedRows[0].stimulusId).to.exist;
+    expect(assignedRows[0].aoiId).to.exist;
+    expect(assignedRows[assignedRows.length - 1].stimulusId).to.exist;
+    expect(assignedRows[assignedRows.length - 1].aoiId).to.exist;
   });
 
   it('assigns stimuli and aois to each row in a real smi file with multiple stimuli', async () => {
     const datafile = Factory.create('smiMultiDatafile');
     datafile.fileFormat = 'smi';
     const rawCSVData = await datafile.getRawCSV();
+
+    expect(rawCSVData[0].stimulusId).to.be.undefined;
+    expect(rawCSVData[0].aoiId).to.be.undefined;
+    expect(rawCSVData[rawCSVData.length - 1].stimulusId).to.be.undefined;
+    expect(rawCSVData[rawCSVData.length - 1].aoiId).to.be.undefined;
+
     const assignedRows = datafile.getAssignedRows(rawCSVData);
 
     expect(assignedRows.length).to.equal(42129); // verified in excel
 
-    // TODO pick back up here 2019-08-23
+    const stimuli = Stimuli.find({ studyId: datafile.studyId });
+    expect(stimuli.count()).to.equal(10); // "Spool 1" through "Spool 10"
+
+    const aois = Aois.find({ studyId: datafile.studyId });
+
+    expect(aois.count()).to.equal(28); // verified in excel
+
+    expect(assignedRows[0].stimulusId).to.exist;
+    expect(assignedRows[0].aoiId).to.exist;
+    expect(assignedRows[assignedRows.length - 1].stimulusId).to.exist;
+    expect(assignedRows[assignedRows.length - 1].aoiId).to.exist;
   });
 });

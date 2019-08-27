41dab9091bf85033f82f103c8f4dc9c7664f9aa0
mattsears18 <mattsears18@gmail.com>
2019-08-26 21:08:47 -0600

    todo

--- collections/Datafiles/imports/getRawCSV.js
+++ collections/Datafiles/imports/getRawCSV.js
@@ -4,4 +4,6 @@ async function getRawCSV() {
   return csv({ delimiter: 'auto' }).fromFile(this.getPathFilename());
 }
 
+// TODO go ahead and filter out the unneeded columns right here to save memory!
+
 export default getRawCSV;

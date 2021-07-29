779d74fd52b73d585f6e823a3a521a70180140d8
mattsears18 <mattsears18@gmail.com>
2021-07-29 18:30:46 -0400

    jstat version fix - WORKING

--- client/partials/plots/regressionStats.js
+++ client/partials/plots/regressionStats.js
@@ -1,9 +1,14 @@
 import { jStat } from 'jStat';
 
 Template.RegressionStats.helpers({
-  linearFit: () => helpers.linearEquation(Template.currentData().x, Template.currentData().y),
-  slope: () => helpers.linearEquation(Template.currentData().x, Template.currentData().y).equation[0],
+  linearFit: () =>
+    helpers.linearEquation(Template.currentData().x, Template.currentData().y),
+  slope: () =>
+    helpers.linearEquation(Template.currentData().x, Template.currentData().y)
+      .equation[0],
   slopeStats: () => 5,
-  intercept: () => helpers.linearEquation(Template.currentData().x, Template.currentData().y).equation[1],
+  intercept: () =>
+    helpers.linearEquation(Template.currentData().x, Template.currentData().y)
+      .equation[1],
   interceptStats: () => 5,
 });
